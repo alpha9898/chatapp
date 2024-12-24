572dac35119913aac35ad2fb6e05a53a3522eee4
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-12-24 12:35:23 +0200

    Enhance chat page layout by adding a message input field; restructure body to use Column and Expanded for better UI organization

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -26,9 +26,41 @@ class ChatPage extends StatelessWidget {
           ],
         ),
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ChatBuble();
-      }),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(itemBuilder: (context, index) {
+              return ChatBuble();
+            }),
+          ),
+          const Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                  borderSide: BorderSide(
+                    color: kPrimaryColor,
+                  ),
+                ),
+                hintText: 'send a message',
+                contentPadding: EdgeInsets.all(16),
+                suffixIcon: Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
