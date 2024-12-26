6a54d28904c1f5132aef72c7a76b30c88357daed
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-12-26 17:37:49 +0200

    Implement message sending functionality in chat page; add Firestore integration and message input field

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF2B475E);
 const KLogo = 'assets/images/scholar.png';
+const KMessagesCollections = 'messages';
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,66 +1,87 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/widgets/chat_buble.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatPage extends StatelessWidget {
   static String id = 'ChatPage';
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(KMessagesCollections);
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              KLogo,
-              height: 50,
-            ),
-            const Text(
-              'Scholar Page',
-              style: TextStyle(
-                color: Colors.white,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['massege']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    KLogo,
+                    height: 50,
+                  ),
+                  const Text(
+                    'Scholar Page',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(itemBuilder: (context, index) {
-              return ChatBuble();
-            }),
-          ),
-          const Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return ChatBuble();
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({
+                        'massege': data,
+                      });
+                      controller.clear();
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(32),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(32),
+                        ),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      hintText: 'send a message',
+                      contentPadding: EdgeInsets.all(16),
+                      suffixIcon: Icon(
+                        Icons.send,
+                        color: kPrimaryColor,
+                      ),
+                    ),
                   ),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32),
-                  ),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
-                  ),
-                ),
-                hintText: 'send a message',
-                contentPadding: EdgeInsets.all(16),
-                suffixIcon: Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
-                ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return Text('Loading...');
+        }
+      },
     );
   }
 }
